c726633f942cba485ef5397a3e644304d174d886
wayneSongwx <waynesongxh@gmail.com>
2024-10-12 18:11:32 -0400

    wayne: 1. add some annotations to pom.xml
    2. update the application.properties (I also leave the old one. You can still use the old one. The difference is that i add a new line: "spring.jpa.hibernate.ddl-auto=update". Now Hibernate (the JPA implementation) manages the creation and updating of our database schema.
    
    3. Update the schema.sql. I write the schema of the current tables we have. I annotate the whole file as the Hibernate will help us create the tables now.
    
    4. optimize the User and Event class. Construct the relationship between User and Events: user can create many events; user can attend multiple events.
    
    5. I create the EventController, EventService, EventRepository

--- src/main/java/com/eventease/eventease_service/model/Organizer.java
+++
@@ -1,9 +0,0 @@
-package com.eventease.eventease_service.model;
-
-import java.io.Serial;
-import java.io.Serializable;
-
-public class Organizer implements Serializable {
-  @Serial
-  private static final long serialVersionUID = 100001L;
-}
