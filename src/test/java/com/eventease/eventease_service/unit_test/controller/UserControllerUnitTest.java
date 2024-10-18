108ad93b65c71ff5bccffaf762761de9da63ef4b
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2024-10-18 19:59:24 -0400

    Remove unused comments from UserControllerUnitTest.java

--- src/test/java/com/eventease/eventease_service/unit_test/controller/UserControllerUnitTest.java
+++ src/test/java/com/eventease/eventease_service/unit_test/controller/UserControllerUnitTest.java
@@ -1,7 +1,6 @@
 package com.eventease.eventease_service.unit_test.controller;
 
 import com.eventease.eventease_service.model.User;
-import com.eventease.eventease_service.repository.UserRepository;
 import com.eventease.eventease_service.service.UserService;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -32,8 +31,6 @@ public class UserControllerUnitTest {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
 
     @BeforeEach
     public void setup() {
@@ -42,10 +39,8 @@ public class UserControllerUnitTest {
     private int getTestUserId() {
         AtomicInteger TestUserId = new AtomicInteger(-1);
 
-        userRepository.findAll().forEach(user -> {
-            if (user.getFirstName().equals("John") && user.getLastName().equals("Doe")) {
-                TestUserId.set(Math.toIntExact(user.getId()));
-            }
+        userService.getUsersByFilter("John", "Doe", null, null, null).forEach(u -> {
+            TestUserId.set(u.getId().intValue());
         });
 
         assert(TestUserId.get() != -1);
@@ -61,8 +56,6 @@ public class UserControllerUnitTest {
         user.setLastName("Doe");
         user.setEmail("john.doe@example.com");
 
-//        doNothing().when(userService).addUser(any(User.class));
-
         mockMvc.perform(post("/api/users/add")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"firstName\": \"John\", \"lastName\": \"Doe\", \"email\": \"john.doe@example.com\"}"))
@@ -71,22 +64,16 @@ public class UserControllerUnitTest {
 
         // confirm user was added, and store its id
         this.getTestUserId();
-
-//        verify(userService, times(1)).addUser(any(User.class));
     }
 
     @Test
     @Order(2)
     public void testGetUsersByFilter() throws Exception {
-//        when(userService.getUsersByFilter(anyString(), anyString(), anyString(), anyString(), any(User.Role.class)))
-//                .thenReturn(new ArrayList<>());  // Assuming returning empty list
-
         mockMvc.perform(get("/api/users/list")
                         .param("firstName", "John")
                         .param("lastName", "Doe"))
                 .andExpect(status().isOk());
 
-//        verify(userService, times(1)).getUsersByFilter(eq("John"), eq("Doe"), isNull(), isNull(), isNull());
     }
 
     @Test
@@ -98,29 +85,22 @@ public class UserControllerUnitTest {
 
         int TestUserId = this.getTestUserId();
 
-//        when(userService.updateUser(eq(1L), any(User.class))).thenReturn("User updated successfully");
-
         mockMvc.perform(patch("/api/users/update/" + TestUserId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"firstName\": \"John\", \"lastName\": \"Doe\"}"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("User updated successfully"));
 
-//        verify(userService, times(1)).updateUser(eq(1L), any(User.class));
     }
 
     @Test
     @Order(4)
     public void testDeleteUser() throws Exception {
-//        doNothing().when(userService).deleteUser(1L);
-
         int TestUserId = this.getTestUserId();
 
         mockMvc.perform(delete("/api/users/delete/" + TestUserId))
                 .andExpect(status().isOk())  // Expecting status 200
                 .andExpect(content().string("User deleted successfully"));  // Expecting success message
-
-//        verify(userService, times(1)).deleteUser(1L);
     }
 
 }
