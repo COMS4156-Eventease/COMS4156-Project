037b24bc77f5403129bde1b00abad4b5b8b9007a
alok27a <alok27a@gmail.com>
2024-10-18 16:28:32 -0400

    Testing checkstyle

--- src/test/java/com/eventease/eventease_service/unit_test/UserControllerUnitTest.java
+++ src/test/java/com/eventease/eventease_service/unit_test/UserControllerUnitTest.java
@@ -5,28 +5,27 @@ import com.eventease.eventease_service.model.User;
 import com.eventease.eventease_service.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+@ExtendWith(MockitoExtension.class)
 public class UserControllerUnitTest {
 
     private MockMvc mockMvc;
 
-   
     @Mock
     private UserService userService;
 
@@ -35,9 +34,9 @@ public class UserControllerUnitTest {
 
     @BeforeEach
     public void setup() {
-        MockitoAnnotations.openMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
     }
+
 
     @Test
     public void testAddNewUser() throws Exception {
