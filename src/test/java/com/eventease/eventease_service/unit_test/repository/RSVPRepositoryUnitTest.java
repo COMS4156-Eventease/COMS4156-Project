0e19d18fde34b0b08e8e261f7f470a80db57678f
Jason Zou <tapjason@gmail.com>
2024-11-22 01:45:17 -0500

    zzz fixing active test profile

--- src/test/java/com/eventease/eventease_service/unit_test/repository/RSVPRepositoryUnitTest.java
+++ src/test/java/com/eventease/eventease_service/unit_test/repository/RSVPRepositoryUnitTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RSVPRepositoryUnitTest {
   @Autowired
