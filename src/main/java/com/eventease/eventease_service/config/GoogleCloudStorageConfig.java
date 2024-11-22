c9bf16fd9ddf6a809067d1b52139e586920657d7
wayneSongwx <waynesongxh@gmail.com>
2024-11-22 14:40:03 -0500

    modify the way connecting to GCS

--- src/main/java/com/eventease/eventease_service/config/GoogleCloudStorageConfig.java
+++ src/main/java/com/eventease/eventease_service/config/GoogleCloudStorageConfig.java
@@ -3,6 +3,7 @@ package com.eventease.eventease_service.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import org.springframework.context.annotation.Bean;
@@ -15,9 +16,9 @@ public class GoogleCloudStorageConfig {
   public Storage storage() throws IOException {
     // Load environment variables from .env file
     //
-//    Dotenv dotenv = Dotenv.configure().load();
-//    String credentialsJson = dotenv.get("GCP_CREDENTIALS");
-    String credentialsJson = System.getenv("GCP_CREDENTIALS");
+    Dotenv dotenv = Dotenv.configure().load();
+    String credentialsJson = dotenv.get("GCP_CREDENTIALS");
+//    String credentialsJson = System.getenv("GCP_CREDENTIALS");
     if (credentialsJson == null || credentialsJson.isEmpty()) {
       throw new IllegalStateException("Environment variable GCP_CREDENTIALS is not set or empty.");
     }
