55d1200986424e0b78d3c47883630f2c0b57fe03
Louis <hl3812@columbia.edu>
2024-11-25 01:37:56 -0500

    Reformatted User endpoints

--- src/main/java/com/eventease/eventease_service/controller/UserController.java
+++ src/main/java/com/eventease/eventease_service/controller/UserController.java
@@ -9,6 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -17,41 +21,100 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/add")
-    public String addNewUser(@RequestBody User user) {
-        userService.addUser(user); // Assuming userService is handling the logic of saving the User object
-        return "User saved successfully";
+    public ResponseEntity<?> addNewUser(@RequestBody User user) {
+        Map<String, Object> response = new HashMap<>();
+        try{
+            user = userService.addUser(user);
+            response.put("success", true);
+            response.put("data", user);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (UserNotExistException e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }  catch (Exception e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        Map<String, Object> response = new HashMap<>();
         try {
             User user = userService.findUserById(id);
-            return new ResponseEntity<>(user, HttpStatus.OK);
+            response.put("success", true);
+            response.put("data", user);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (UserNotExistException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/list")
-    public Iterable<User> getUsersByFilter(
+    public ResponseEntity<?> getUsersByFilter(
             @RequestParam(required = false) String firstName,
             @RequestParam(required = false) String lastName,
             @RequestParam(required = false) String email,
             @RequestParam(required = false) String phone,
             @RequestParam(required = false) User.Role role)
     {
-        return userService.getUsersByFilter(firstName, lastName, email, phone, role);
+        Map<String, Object> response = new HashMap<>();
+        try{
+            Iterable<User> users = userService.getUsersByFilter(firstName, lastName, email, phone, role);
+            response.put("success", true);
+            response.put("data", users);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch ( Error error) {
+            response.put("success", false);
+            response.put("error", error.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @PatchMapping("/update/{id}")
-    public String updateUser(@PathVariable Long id, @RequestBody User user) {
-        return userService.updateUser(id, user);
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User user) {
+        Map<String, Object> response = new HashMap<>();
+        try{
+            userService.updateUser(id, user);
+            response.put("success", true);
+            response.put("data", user);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (UserNotExistException e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return "User deleted successfully";
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        Map<String, Object> response = new HashMap<>();
+        try{
+            userService.deleteUser(id);
+            response.put("success", true);
+            response.put("data", null);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (UserNotExistException e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
